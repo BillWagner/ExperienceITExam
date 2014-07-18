e70bf22b623d197028b341d4950c9bb5b6bc9932
William O'Sullivan <williamosullivan@gmail.com>
2014-07-18 16:31:33 -0400

    Out of Time, Final Commit
    Done

---
+++ Dice/Models/Dice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Dice.Models
+{
+    public class Dice
+    {
+        [Key]
+        public int die1 { get; set; }
+        public int die2 { get; set; }
+
+        public class DiceContext : DbContext
+        {
+        }
+    }
+}
