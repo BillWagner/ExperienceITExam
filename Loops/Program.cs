3f6dce1eb79cff9efcc240f39f14d6473cec51b5
itchyfangaz <mrjessecunningham@gmail.com>
2014-07-18 16:38:45 -0400

    ExperienceITEXAM
    Forked

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -61,6 +61,9 @@ namespace Loops
 
         static void Main(string[] args)
         {
+            foreach
+            
+          
             //1. Write code to print all the names of all the studnets in the collection above.
             // 2. Modify the Student class to provide a method to print each students name.
             // 3. Use the method from (2) to print the students' names.
