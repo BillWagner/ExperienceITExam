88f36eb04a8fb55f09793c66a49615893c1232f4
Falcon760 <sfalcon266@hotmail.com>
2014-07-18 15:18:53 -0400

    3:18

--- DiceRoll/Models/Dice.cs
+++ DiceRoll/Models/Dice.cs
@@ -5,7 +5,22 @@ using System.Web;
 
 namespace DiceRoll.Models
 {
+    
     public class Dice
     {
+        Random number = new Random();
+        Random number2 = new Random();
+        public int DiceId { get; set; }
+      
+
+        public void Roll()
+        {
+            number.Next(0, 6);
+            number2.Next(0, 6);
+            Console.WriteLine("{0),{1}", number, number2);
+
+        }
     }
+   
+
 }
